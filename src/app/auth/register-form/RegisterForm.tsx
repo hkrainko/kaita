64c3dbde0cc4c7cd1cccfc2391fe15d4a35ce9c2
hkrainko <>
2021-04-28 00:13:16 +0800

    no message

--- src/app/auth/register-form/RegisterForm.tsx
+++ src/app/auth/register-form/RegisterForm.tsx
@@ -1,5 +1,15 @@
-import {Box, createStyles, makeStyles, Theme} from "@material-ui/core";
+import {
+    Box,
+    createStyles,
+    FormControl,
+    FormGroup,
+    FormHelperText, Grid, Input,
+    InputLabel,
+    makeStyles,
+    Theme
+} from "@material-ui/core";
 import {useHistory} from "react-router-dom";
+import React from "react";
 
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -16,7 +26,25 @@ export default function RegisterForm() {
 
     return (
         <Box mt={4} className={classes.root}>
-            <h1>RegisterForm</h1>
+            <Grid container spacing={3} justify={"center"}>
+                <Grid item xs={10} md={8}>
+                    <h1>Register As </h1>
+                </Grid>
+                <Grid item xs={10} md={8}>
+                    <FormGroup>
+                        <FormControl>
+                            <InputLabel htmlFor="my-input">Email address</InputLabel>
+                            <Input id="my-input" aria-describedby="my-helper-text"/>
+                            <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+                        </FormControl>
+                        <FormControl>
+                            <InputLabel htmlFor="my-input">Email address</InputLabel>
+                            <Input id="my-input" aria-describedby="my-helper-text"/>
+                            <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+                        </FormControl>
+                    </FormGroup>
+                </Grid>
+            </Grid>
         </Box>
-    )
+    );
 }
