399d20667933eaea6cb9f6613edf867de6e58c08
hkrainko <>
2021-05-09 21:40:13 +0800

    no message

--- src/app/open-commission/OpenCommissionCard.tsx
+++ src/app/open-commission/OpenCommissionCard.tsx
@@ -1,20 +1,25 @@
 import {
     Box,
-    Button,
     Card,
     CardActionArea,
     CardActions,
     CardContent,
     CardHeader,
     CardMedia,
+    Collapse,
     createStyles,
+    IconButton,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
     makeStyles,
     StandardProps,
-    Theme,
-    Typography
+    Theme
 } from "@material-ui/core";
-import {AttachMoneyOutlined} from "@material-ui/icons";
+import {DateRangeOutlined, ExpandMoreOutlined, MmsOutlined, RateReviewOutlined} from "@material-ui/icons";
 import {OpenCommission} from "../../domain/open-commission/model/open-commission";
+import {useState} from "react";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -22,7 +27,24 @@ const useStyles = makeStyles((theme: Theme) =>
             marginTop: 0,
             marginBottom: 0,
             fontSize: '16px',
-        }
+        },
+        listItem: {
+
+        },
+        listItemIcon: {
+
+        },
+        expand: {
+            transform: 'rotate(0deg)',
+            marginLeft: 'auto',
+            transition: theme.transitions.create('transform', {
+                duration: theme.transitions.duration.shortest,
+            }),
+        },
+        expandOpen: {
+            marginLeft: 'auto',
+            transform: 'rotate(180deg)',
+        },
     }),
 );
 
@@ -33,6 +55,7 @@ interface Props extends StandardProps<any, any> {
 export default function OpenCommissionCard({openCommission, ...props}: Props) {
 
     const classes = useStyles(props.className)
+    const [expanded, setExpanded] = useState(false)
 
     return (
         <Card>
@@ -42,7 +65,6 @@ export default function OpenCommissionCard({openCommission, ...props}: Props) {
                         <Box display={"flex"} justifyContent={"space-between"}>
                             <p className={classes.title}>{openCommission.title}</p>
                             <Box display={"flex"}>
-                                <AttachMoneyOutlined/>
                                 <p className={classes.title}>
                                     {`${openCommission.price?.amount} ${openCommission.price?.currency}`}
                                 </p>
@@ -54,22 +76,46 @@ export default function OpenCommissionCard({openCommission, ...props}: Props) {
                     component="img"
                     alt="Contemplative Reptile"
                     height="140"
-                    image="/static/images/cards/contemplative-reptile.jpg"
+                    image={`http://192.168.64.12:31398/${openCommission.sampleImagePaths[0]}`}
                     title="Contemplative Reptile"
                 />
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        {openCommission.desc}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {openCommission.dayNeed?.from}~{openCommission.dayNeed?.to} 天
-                    </Typography>
+                    <List>
+                        <ListItem className={classes.listItem}>
+                            <ListItemIcon className={classes.listItemIcon}>
+                                <DateRangeOutlined/>
+                            </ListItemIcon>
+                            <ListItemText primary="需時" secondary={`${openCommission.dayNeed?.from} ~ ${openCommission.dayNeed?.to} 日`}/>
+                        </ListItem>
+                        <ListItem className={classes.listItem}>
+                            <ListItemIcon className={classes.listItemIcon}>
+                                <RateReviewOutlined/>
+                            </ListItemIcon>
+                            <ListItemText primary="草稿可修改次數" secondary={`${openCommission.timesAllowedDraftToChange} 次`}/>
+                        </ListItem>
+                        <ListItem className={classes.listItem}>
+                            <ListItemIcon className={classes.listItemIcon}>
+                                <MmsOutlined/>
+                            </ListItemIcon>
+                            <ListItemText primary="完成品可修改次數" secondary={`${openCommission.timesAllowedCompletionToChange} 次`}/>
+                        </ListItem>
+                    </List>
                 </CardContent>
             </CardActionArea>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                    123
+                </CardContent>
+            </Collapse>
             <CardActions>
-                <Button size="small" color="primary">
-                    Edit
-                </Button>
+                <IconButton
+                    className={`${expanded ? classes.expandOpen: classes.expand}`}
+                    onClick={() => setExpanded(!expanded)}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                >
+                    <ExpandMoreOutlined />
+                </IconButton>
             </CardActions>
         </Card>
     )
