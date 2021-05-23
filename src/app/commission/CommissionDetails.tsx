33704103d6bf3bc640985c08e99a3ccdbd8fe876
hkrainko <>
2021-05-23 14:41:57 +0800

    no message

--- src/app/commission/CommissionDetails.tsx
+++ src/app/commission/CommissionDetails.tsx
@@ -1,12 +1,17 @@
 import {
     Box,
+    Button,
+    Checkbox,
     createStyles,
     Dialog,
+    DialogActions,
     DialogContent,
-    DialogContentText,
     DialogTitle,
+    List,
+    ListItem,
+    ListItemText,
     makeStyles,
-    Theme, Typography
+    Theme
 } from "@material-ui/core";
 import {useAppDispatch} from "../hooks";
 import {Commission} from "../../domain/commission/model/commission";
@@ -48,34 +53,126 @@ export default function CommissionDetail({commission, open, onClose, ...props}: 
                 委托資料
             </DialogTitle>
             <DialogContent>
-                <DialogContentText>
-                    {commission.id}
-                </DialogContentText>
-                <DialogContentText>
-                    <label>開放委托</label>
-                    <Typography>{commission.openCommissionId}</Typography>
-                </DialogContentText>
-                <DialogContentText>
-                    <label>繪師</label>
-                    <Typography>{commission.artistName}@{commission.artistId}</Typography>
-                </DialogContentText>
-                <DialogContentText>
-                    <label>委托人</label>
-                    <Typography>{commission.requesterName}@{commission.requesterId}</Typography>
-                </DialogContentText>
-                <DialogContentText>
-                    <label>範例</label>
-                    <Box display="flex" width="100%">
-                        {commission.refImagePaths.map((path, index) => (
-                            <div className={classes.sampleImg} key={index}>
-                                <AuthImage
-                                    src={`http://192.168.64.12:31398/${path}`}
-                                    alt="範例"/>
-                            </div>
-                        ))}
-                    </Box>
-                </DialogContentText>
+                <List>
+                    <ListItem>
+                        <ListItemText
+                            primary="委托編號"
+                            secondary={commission.id}
+                        />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText
+                            primary="開放委托編號"
+                            secondary={commission.openCommissionId}
+                        />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText
+                            primary="繪師"
+                            secondary={`${commission.artistName}@${commission.artistId}`}
+                        />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText
+                            primary="委托人"
+                            secondary={`${commission.requesterName}@${commission.requesterId}`}
+                        />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="詳細內容"
+                                      secondary={commission.desc}/>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="參考"
+                                      secondary={
+                                          <Box display="flex" width="100%">
+                                              {commission.refImagePaths.map((path, index) => (
+                                                  <div className={classes.sampleImg} key={index}>
+                                                      <AuthImage
+                                                          src={`http://192.168.64.12:31398/${path}`}
+                                                          alt="範例"/>
+                                                  </div>
+                                              ))}
+                                          </Box>
+                                      }/>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="期限"
+                                      secondary={`${commission.dayNeed}日`}/>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="付款方式"
+                                      secondary={commission.paymentMethod}/>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="詳細內容"
+                                      secondary={commission.desc}/>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="尺吋 (闊 x 高)"
+                                      secondary={
+                                          commission.size
+                                              ? `${commission.size.width} x ${commission.size.height} ${commission.size.unit}`
+                                              : "未指定"
+                                      }/>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="解析度"
+                                      secondary={
+                                          commission.resolution
+                                              ? `${commission.resolution} ppi`
+                                              : "未指定"
+                                      }/>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="輸出格式"
+                                      secondary={
+                                          commission.exportFormat
+                                              ? `${commission.exportFormat}`
+                                              : "未指定"
+                                      }/>
+                    </ListItem>
+                    {
+                        commission.bePrivate &&
+                        <ListItem>
+                            <Checkbox
+                                checked={commission.isR18}
+                                name="isR18"
+                                color="default"
+                                disabled
+                            />
+                            <p>成人向委托</p>
+                        </ListItem>
+                    }
+                    {
+                        commission.bePrivate &&
+                        <ListItem>
+                            <Checkbox
+                                checked={commission.bePrivate}
+                                name="bePrivate"
+                                color="default"
+                            />
+                            <p>不公開完成品</p>
+                        </ListItem>
+                    }
+                    {
+                        commission.bePrivate &&
+                        <ListItem>
+                            <Checkbox
+                                checked={commission.anonymous}
+                                name="anonymous"
+                                color="default"
+                            />
+                            <p>匿名委托</p>
+                        </ListItem>
+                    }
+                </List>
             </DialogContent>
+            <DialogActions>
+                <Button variant="contained" color="default" onClick={onClose} fullWidth>
+                    確定
+                </Button>
+            </DialogActions>
         </Dialog>
     )
 }
