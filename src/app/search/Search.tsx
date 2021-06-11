c84fe4e4fad5fa2f2b380251f815fa724fa850ac
hkrainko <>
2021-06-12 05:36:21 +0800

    no message

--- src/app/search/Search.tsx
+++ src/app/search/Search.tsx
@@ -29,8 +29,48 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 interface FilterSorter {
+    type: SearchType
     filter: SearchFilter,
-    sorter: SearchSorter
+    sorter: SearchSorter,
+    currentPage: number
+}
+
+const getInitFilterSorter = (type: SearchType): FilterSorter => {
+    switch (type) {
+        case SearchType.OpenCommissions:
+            return {
+                type: SearchType.OpenCommissions,
+                filter: {
+                    type: SearchType.OpenCommissions
+                },
+                sorter: {
+                    type: SearchType.OpenCommissions
+                },
+                currentPage: 1
+            }
+        case SearchType.Artists:
+            return {
+                type: SearchType.Artists,
+                filter: {
+                    type: SearchType.Artists
+                },
+                sorter: {
+                    type: SearchType.Artists
+                },
+                currentPage: 1
+            }
+        case SearchType.Artworks:
+            return {
+                type: SearchType.Artworks,
+                filter: {
+                    type: SearchType.Artworks
+                },
+                sorter: {
+                    type: SearchType.Artworks
+                },
+                currentPage: 1
+            }
+    }
 }
 
 interface Props extends StandardProps<any, any> {
@@ -41,15 +81,15 @@ export default function Search(props: Props) {
     const classes = useStyles()
 
     const location = useLocation()
-    const [filterSorter, setFilterSorter] = useState<FilterSorter | null>(null)
+    const [filterSorter, setFilterSorter] = useState<FilterSorter>(getInitFilterSorter(SearchType.OpenCommissions))
     const query = new URLSearchParams(location.search)
     const searchUseCase = useInjection<SearchUseCase>(TYPES.SearchUseCase)
     const searchType = query.get('t')
     const searchText = query.get('s')
     const dispatch = useAppDispatch()
-    const [page, setPage] = useState<number>(1)
+    // const [page, setPage] = useState<number>(1)
 
-    const fetchDataByType = useCallback((searchType: SearchType, currentPage: number) => {
+    const fetchDataByType = useCallback((filterSorter: FilterSorter) => {
         if (!searchText) {
             return
         }
@@ -61,29 +101,32 @@ export default function Search(props: Props) {
                     sorter: filterSorter?.sorter.type === SearchType.OpenCommissions ? filterSorter.sorter : {
                         lastUpdatedTime: SortOrder.Descending,
                         type: SearchType.OpenCommissions
-                    }, currentPage,
+                    }, currentPage: filterSorter.currentPage,
                     pageSize: 9
                 }))
                 break
             default:
                 break
         }
-    }, [dispatch, filterSorter?.filter, filterSorter?.sorter, searchText])
+    }, [dispatch, searchText])
 
     useEffect(() => {
-        setPage(1)
+        setFilterSorter({...filterSorter, currentPage: 1})
     }, [searchType, searchText])
 
     useEffect(() => {
         if (!searchType) {
             return
         }
-        fetchDataByType(searchType as SearchType, page)
-    }, [fetchDataByType, page, searchType])
+        fetchDataByType(filterSorter)
+    }, [fetchDataByType, filterSorter, searchType])
 
     const onConfirmSelection = useCallback(<T extends SearchFilter, U extends SearchSorter>(filter: T, sorter: U) => {
         setFilterSorter({
-            filter, sorter
+            type: searchType as SearchType,
+            filter,
+            sorter,
+            currentPage: 1
         })
         console.log(`filter: ${JSON.stringify(filter)}, sorter: ${JSON.stringify(sorter)}`)
     }, [])
@@ -105,7 +148,7 @@ export default function Search(props: Props) {
     return (
         <Container className={classes.root}>
             {getSelector()}
-            <SearchResultList searchType={SearchType.OpenCommissions} onLoadMore={() => setPage(page + 1)}/>
+            <SearchResultList searchType={SearchType.OpenCommissions} onLoadMore={() => setFilterSorter({...filterSorter, currentPage: filterSorter.currentPage + 1})}/>
         </Container>
     )
 }
